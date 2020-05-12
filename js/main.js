fe6f2904ff306b04a0f0d4f76aac2ec1d29316d3
Ological <Ological@users.noreply.github.com>
2020-05-11 20:14:30 -0400

    rework

--- js/main.js
+++ js/main.js
@@ -93,9 +93,9 @@ $(document).ready(function() {
 
   $('.services-carousel').owlCarousel({
       autoplay: true,
-      loop: true,
+      loop: false,
       margin: 20,
-      dots: true,
+      dots: false,
       nav: false,
       responsiveClass: true,
       responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
